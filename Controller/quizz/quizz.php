1e735bf4092c30d8876159fa9fc5af8b0aa9ac5a
Mohamed Aziz Ghadhoun <113785275+celestial-unit@users.noreply.github.com>
2023-11-07 20:20:22 +0100

    Add files via upload

---
+++ Controller/quizz/quizz.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Quizz</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>QuickMaths Quizz</title>
+</head>
+
+<body>
+
+    <div class="main">
+        <div class="container">
+            <h1 class="title">QuickMaths Quizz</h1>
+            <div class="card">
+                <div class="card-head">
+                    <span class="big">Bienvenue !</span>
+                </div>
+                <hr>
+                <div class="card-body">
+                    <p>Qu'est-ce que <strong>QuickMaths Quizz</strong> ?</p>
+                    <p class="text-center">Il s'agit d'un petit jeu-questionnaire sur les mathématiques spécialement conçu pour la Semaine des mathématiques.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="questions">
+        <div class="card">
+            <div class="card-head">
+                <span class="normal">Question n°1</span>
+                <span class="question">Combien font <code>2+2</code> ?</span>
+            </div>
+            <hr>
+            <div class="card-body">
+                <input type="text" class="input-answer" placeholder="0">
+                <button class="button-answer">Soumettre</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-head">
+                <span class="normal">Question n°2</span>
+                <span class="question">Développer <code>2*(6+9)</code> ?</span>
+            </div>
+            <hr>
+            <div class="card-body">
+                <select class="select-answer">
+                    <option value="2*6+2*9">2*6+2*9</option>
+                    <option value="2*6-2*9">2*6-2*9</option>
+                </select>
+                <button class="button-answer">Soumettre</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-head">
+                <span class="normal">Question n°2</span>
+                <span class="question">Développer <code>2*(6+9)</code> ?</span>
+            </div>
+            <hr>
+            <div class="card-body">
+                <select class="select-answer">
+                    <option value="2*6+2*9">2*6+2*9</option>
+                    <option value="2*6-2*9">2*6-2*9</option>
+                </select>
+                <button class="button-answer">Soumettre</button>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+</body>
+</html>
