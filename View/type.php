8ab2a29ac49733f2b20d5d879d7c0c3be6cf43a9
azizchouaia2 <136696044+azizchouaia2@users.noreply.github.com>
2023-11-22 08:56:28 +0100

    Add files via upload

---
+++ View/type.php
@@ -0,0 +1,41 @@
+<?php
+class type
+{
+    private ?int $id_types= null;
+    private ?string $nom_types = null;
+
+    
+
+  
+        public function __construct(
+            int $id_types,
+            string $nom_types,
+            
+        
+    ) {
+        $this->id_types = $id_types;
+        $this->nom_types = $id_types;
+
+        
+    }
+    public function getIdtypestage()
+    {
+        return $this->id_types;
+    }
+
+
+    public function getNomtype()
+    {
+        return $this->nom_types;
+    }
+
+  
+
+    public function setDescription($nom_types)
+    {
+        $this->nom_types= $nom_types;
+        return $this;
+    }
+
+    
+}
